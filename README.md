517b600afd9365ae20bb710d3e1933124cc824c9
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-07-23 16:53:37 -0500

    [add] Binary Search Solution

--- README.md
+++ README.md
@@ -4,13 +4,14 @@ Solving some algorithms, then try to calculate the Big-O complexity
 
 ## Math Algorithms
 
-1. Fibonacci Serie
+1. Fibonacci serie
 2. Factorial
 3. Prime number
-4. Power of Two
-5. Recursive Fibonacci
-5. Recursive Factorial
+4. Power of two
+5. Recursive fibonacci
+5. Recursive factorial
 
 ## Search Algorithms
 
-1. 
+1. Linear search
+2. Binary search
--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-// Big-O = O(n) :: linear time complexity
-const linearSearch = (arr, target) => {
-  for (let i=0; i<arr.length; i++) {
-    if (arr[i] === target) {
-      return i;
+// Big-O = O(logn) :: logiritmic time complexity
+const binarySearch = (sortedArr, target) => {
+  let leftIndex = 0;
+  let rightIndex = sortedArr.length - 1;
+
+  while(leftIndex <= rightIndex) {
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (target === sortedArr[middleIndex]) {
+      return middleIndex;
+    }
+    if (target < sortedArr[middleIndex]) {
+      rightIndex = middleIndex - 1;
+    } else {
+      leftIndex = middleIndex + 1;
     }
   }
   return -1;
 };
 
-console.log(linearSearch([2, 5, 7, 1, 0, 3], 5)); // 1
-console.log(linearSearch([4, 2, 11, 20, 1], 4)); // 0
-console.log(linearSearch([5, 1, 4, 8, 9, 10], 12)); // -1
+console.log(binarySearch([-5, 2, 4, 6, 10], 10)); // 4
+console.log(binarySearch([-5, 2, 4, 6, 10], 6)); // 3
+console.log(binarySearch([-5, 2, 4, 6, 10], 20)); // -1
